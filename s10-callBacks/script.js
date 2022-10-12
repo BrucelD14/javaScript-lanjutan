4556c1b20f381de70316ace034f726e92a5312e8
BrucelD14 <samuderaduta144@gmail.com>
2022-10-12 13:25:03 +0700

    belajar callbacks

---
+++ s10-callBacks/script.js
@@ -0,0 +1,50 @@
+// Callback
+// Synchronous Callback
+
+// function halo(nama) {
+//     alert(`Halo, ${nama}`);
+// }
+
+// function tampilkanPesan(callback) {
+//     const nama = prompt('Masukkan Nama : ');
+//     callback(nama);
+// }
+
+// tampilkanPesan(nama => alert(`Halo, ${nama}`));
+
+
+
+// const mhs = [
+//     {
+//         "nama": "Brucel D. Samudera",
+//         "nim": "200170094",
+//         "jurusan": "Teknik Informatika",
+//         "telepon": "088261005473"
+//     },
+//     {
+//         "nama": "Fajar Chan",
+//         "nim": "200170130",
+//         "jurusan": "Teknik Informatika",
+//         "telepon": "088861005411"
+//     },
+//     {
+//         "nama": "Nefo Preyandre",
+//         "nim": "200170080",
+//         "jurusan": "Teknik Informatika",
+//         "telepon": "082161003303"
+//     }
+// ];
+
+// console.log('mulai');
+// mhs.forEach(m => {
+//     for(let i = 0; i < 10000000; i++){
+//         let date = new Date();
+//     }
+
+//     console.log(m.nama)
+// });
+// console.log('selesai');
+
+
+
+// Asynchronous Callback
