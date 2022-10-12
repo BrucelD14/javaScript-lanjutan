1675e58406021467c75660a42da84e7f0038eadb
BrucelD14 <samuderaduta144@gmail.com>
2022-10-12 22:53:38 +0700

    menyambung ke API

--- callBacks-latihan/script.js
+++ callBacks-latihan/script.js
@@ -1,3 +1,10 @@
 $.ajax({
-    url: ''
-})
+    url: 'https://api.themoviedb.org/3/movie/76341?api_key=6c2abd54075d8a09f315ac2769e60c4e',
+    success: results => {
+        console.log(results);
+    },
+    error: (e) => {
+        // jika error
+        console.log(e.responseText);
+    }
+});
