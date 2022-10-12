66862527d33f7921c53e7bcad4c67517ccc0e0c5
BrucelD14 <samuderaduta144@gmail.com>
2022-10-12 23:35:58 +0700

    menambah modals

--- callBacks-latihan/script.js
+++ callBacks-latihan/script.js
@@ -1,7 +1,22 @@
 $.ajax({
     url: 'https://api.themoviedb.org/3/movie/76341?api_key=6c2abd54075d8a09f315ac2769e60c4e',
     success: results => {
-        console.log(results);
+        const movies = results.genres;
+        console.log(movies);
+        let cards = '';
+        movies.forEach(m => {
+            cards += `<div class="col-md-4 my-3">
+                        <div class="card">
+                            <img src="" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">${m.name}</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">${m.id}</h6>
+                                <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#movieDetailModal">Show Details</a>
+                            </div>
+                        </div>
+                    </div>`;
+        });
+        $('.movie-container').html(cards);
     },
     error: (e) => {
         // jika error
