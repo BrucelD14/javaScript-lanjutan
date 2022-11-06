4fac6a50e1be2749c548e4e459e94efbcbf9bab7
BrucelD14 <samuderaduta144@gmail.com>
2022-11-06 12:21:29 +0700

    leaen prototype, add method using prototype

--- s3-Prototype/script.js
+++ s3-Prototype/script.js
@@ -21,31 +21,61 @@
 
 // 2. Function Declaration
 
-const methodMahasiswa = {
-  makan: function (porsi) {
-    this.energi += porsi;
-    console.log(`halo ${this.nama}, selamat maam`);
-  },
-  main: function (jam) {
-    this.energi -= jam;
-    console.log(`Halo ${this.nama}, selamat mainn`);
-  },
+// const methodMahasiswa = {
+//   makan: function (porsi) {
+//     this.energi += porsi;
+//     console.log(`halo ${this.nama}, selamat maam`);
+//   },
+//   main: function (jam) {
+//     this.energi -= jam;
+//     console.log(`Halo ${this.nama}, selamat mainn`);
+//   },
 
-  tidur: function(jam) {
-    this.energi += jam * 2
-    console.log(`Halo ${this.nama}, selamat tidur`);
-  }
-};
+//   tidur: function(jam) {
+//     this.energi += jam * 2
+//     console.log(`Halo ${this.nama}, selamat tidur`);
+//   }
+// };
+// function Mahasiswa(nama, energi) {
+//   let mahasiswa = Object.create(methodMahasiswa);
+//   mahasiswa.nama = nama;
+//   mahasiswa.energi = energi;
+
+//   return mahasiswa;
+// }
+
+// let bucel = Mahasiswa("Brucel", 10);
+// let vio = Mahasiswa("Violita", 18);
+
+
+
 function Mahasiswa(nama, energi) {
-  let mahasiswa = Object.create(methodMahasiswa);
-  mahasiswa.nama = nama;
-  mahasiswa.energi = energi;
-
-  return mahasiswa;
+  this.nama = nama;
+  this.energi = energi;
 }
 
-let bucel = Mahasiswa("Brucel", 10);
-let vio = Mahasiswa("Violita", 18);
+Mahasiswa.prototype.makan = function(porsi) {
+  this.energi += porsi;
+  return `Halo ${this.nama}, selamat makan!`
+}
+
+Mahasiswa.prototype.main = function(jam) {
+  this.energi -= jam;
+  return `Halo ${this.nama}, selamat bermain!`
+}
+
+Mahasiswa.prototype.tidur = function(jam) {
+  this.energi += jam * 2;
+  return `Halo ${this.nama}, selamat turuu!`
+}
+
+let bucel = new Mahasiswa('Brucel', 10);
+
+
+
+
+
+
 
 // 3. Constructor Function
 // keyword new
