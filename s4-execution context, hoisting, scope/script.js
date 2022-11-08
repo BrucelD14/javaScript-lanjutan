9ac0f0a36e7fe4a112925394c8eb27822707e303
BrucelD14 <samuderaduta144@gmail.com>
2022-11-08 14:58:57 +0700

    learn creation phase

--- s4-execution context, hoisting, scope/script.js
+++ s4-execution context, hoisting, scope/script.js
@@ -1,2 +1,10 @@
 // execution context, hoisting & scope
 
+// var nama = "Brucel"; 
+
+// creation phase pada global context
+// nama var = undefined
+// nama function = fn()
+// hoisting
+// window = global object
+// this = window
