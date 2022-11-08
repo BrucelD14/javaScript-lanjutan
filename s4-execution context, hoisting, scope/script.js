18a74583f535466cd4c39912b22b5f2a6496c1a0
BrucelD14 <samuderaduta144@gmail.com>
2022-11-08 15:19:39 +0700

    learn hoisting

--- s4-execution context, hoisting, scope/script.js
+++ s4-execution context, hoisting, scope/script.js
@@ -1,5 +1,6 @@
 // execution context, hoisting & scope
 
+// console.log(nama);
 // var nama = "Brucel"; 
 
 // creation phase pada global context
@@ -8,3 +9,31 @@
 // hoisting
 // window = global object
 // this = window
+
+// execution phase
+
+
+// var nama = 'Brucel';
+// var umur = 20;
+// console.log(sayHello());
+
+// function sayHello() {
+//     return(`halo nama saya ${nama}, saya ${umur} tahun`);
+// }
+
+// function membuat local execution context
+// yang di dalamnya terdapat creation dan execution phase
+// window
+// arguments
+// hoisting
+
+
+var nama = 'Brucel D.';
+var username = '_samudera';
+
+function cetakURL(username) {
+    var instagramURL = 'http://instagram.com/';
+    return instagramURL + username;
+}
+
+console.log(cetakURL(username));
