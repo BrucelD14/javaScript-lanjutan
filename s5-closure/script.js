09c4c3971771ebd759be651d0bb7a387bdca507c
BrucelD14 <samuderaduta144@gmail.com>
2022-11-12 20:11:10 +0700

    learn closure more case

--- s5-closure/script.js
+++ s5-closure/script.js
@@ -1,9 +1,40 @@
-function init() {
-//   let nama = "Brucel";
-  return function (nama) {
-    console.log(nama);
-  }
-}
-let panggilNama = init();
-panggilNama('brucel');
-panggilNama('vioo');
+// function init() {
+// //   let nama = "Brucel";
+//   return function (nama) {
+//     console.log(nama);
+//   }
+// }
+// let panggilNama = init();
+// panggilNama('brucel');
+// panggilNama('vioo');
+
+
+
+
+// function ucapkanSalam(waktu) {
+//     return function(nama) {
+//         console.log(`Halo ${nama}, Selamat ${waktu}. Semoga harimu menyenangkan!`);
+//     }
+// }
+
+// let selamatPagi = ucapkanSalam('pagi')
+// let selamatSiang = ucapkanSalam('siang')
+// let selamatMalam = ucapkanSalam('malam')
+
+// console.dir(selamatMalam('brucel'))
+
+
+let add = (function() {
+    let counter = 0;
+    return function() {
+        return ++counter;
+    }
+})();
+
+counter = 100;
+
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
