68d7851135f827b076fd47e24e1e49a9662abaf6
BrucelD14 <samuderaduta144@gmail.com>
2022-11-18 21:01:52 +0700

    add style.css

--- s7-this-arrow-function/script.js
+++ s7-this-arrow-function/script.js
@@ -22,7 +22,7 @@
 // console.log(tampilNama());
 
 
-let mahasiswa = ['brucel', 'vio', 'samudera'];
+// let mahasiswa = ['brucel', 'vio', 'samudera'];
 
 // let jumlahHuruf = mahasiswa.map(function(nama) {
 //     return nama.length;
@@ -33,5 +33,65 @@ let mahasiswa = ['brucel', 'vio', 'samudera'];
 // let jumlahHuruf = mahasiswa.map(nama => nama.length);
 // console.log(jumlahHuruf);
 
-let jumlahHuruf = mahasiswa.map(nama => ({ nama, jlhHuruf: nama.length }))
-console.table(jumlahHuruf);
+// let jumlahHuruf = mahasiswa.map(nama => ({ nama, jlhHuruf: nama.length }))
+// console.table(jumlahHuruf);
+
+
+
+
+// KONSEP THIS PADA ARROW FUNCTION
+
+// Constructor Function
+// const Mahasiswa = function() {
+//     this.nama = 'brucel',
+//     this.umur = 33,
+//     this.sayHello = function() {
+//         console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun`);
+//     }
+// }
+
+// const brucel = new Mahasiswa();
+
+
+
+// Arrow Function
+// const Mahasiswa = function() {
+//     this.nama = 'brucel',
+//     this.umur = 33,
+//     this.sayHello = () => {
+//         console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun`);
+//     }
+// }
+
+
+// Object Literal
+// const mhs1 = {
+//     nama: 'brucel',
+//     umur: 33,
+//     sayHello: () => {
+//         // console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun`);
+//         console.log(this);
+//     }
+// }
+
+
+
+// const Mahasiswa = function() {
+//     this.nama = 'brucel',
+//     this.umur = 20,
+//     this.sayHello = function() {
+//         console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun`);
+//     }
+
+//     setInterval(() => {
+
+//         console.log(this.umur++);
+
+//     }, 800)
+
+// }
+
+// const brucel = new Mahasiswa();
+
+
+
