d95930569f0ed346903e9e32124a835471ec0cbf
BrucelD14 <samuderaduta144@gmail.com>
2022-11-18 21:37:21 +0700

    learn study case arrow case

--- s7-this-arrow-function/script.js
+++ s7-this-arrow-function/script.js
@@ -95,3 +95,17 @@
 
 
 
+const box = document.querySelector('.box');
+box.addEventListener('click',function() {
+    let satu = 'size'
+    let dua = 'caption'
+
+    if(this.classList.contains(satu)) {
+        [satu, dua] = [dua,satu]
+    }
+
+    this.classList.toggle(satu);
+    setTimeout(() => {
+        this.classList.toggle(dua)
+    }, 600)
+})
