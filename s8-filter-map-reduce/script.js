9a1002b7183997b48bb7136922fe366d2b5f7a43
BrucelD14 <samuderaduta144@gmail.com>
2022-11-28 20:14:24 +0700

    learn map

--- s8-filter-map-reduce/script.js
+++ s8-filter-map-reduce/script.js
@@ -11,7 +11,18 @@ const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
 // console.log(newAngka);
 
 // filter
-const newAngka = angka.filter(function (a) {
-  return a >= 3;
-});
-console.log(newAngka);
+// const newAngka = angka.filter(function (a) {
+//   return a >= 3;
+// });
+// console.log(newAngka);
+
+// map
+// kalikan semua angka dengan 2
+// const newAngka = angka.map((a) => a * 2);
+// console.log(angka);
+// console.log(newAngka);
+
+
+// reduce
+// jumlahkan seluruh elemen pada array
+const newAngka = angka.reduce((accumulator, currentValue))
