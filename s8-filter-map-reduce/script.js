4631c31fecf80802de6cb6aeda6a9dfbc3760f04
BrucelD14 <samuderaduta144@gmail.com>
2022-11-28 20:23:10 +0700

    learn method chaining

--- s8-filter-map-reduce/script.js
+++ s8-filter-map-reduce/script.js
@@ -29,4 +29,14 @@ const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
 // );
 // console.log(newAngka);
 
+// method chaining
+// cari angka > 5
+// kalikan 3
+// jumlahkan
 
+const hasil = angka
+  .filter((a) => a > 5) // 8, 9, 9
+  .map((a) => a * 3) // 24, 27, 27
+  .reduce((acc, cur) => acc + cur); // 78
+
+console.log(hasil);
